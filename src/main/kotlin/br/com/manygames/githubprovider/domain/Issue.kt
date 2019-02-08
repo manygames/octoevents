86518a80ef574e43210adf8765462c071d4e7d58
manygames <wearemanygames@gmail.com>
2019-02-08 17:07:38 -0200

    limpando comentarios e resquicios do spring

--- src/main/kotlin/br/com/manygames/githubprovider/domain/Issue.kt
+++ src/main/kotlin/br/com/manygames/githubprovider/domain/Issue.kt
@@ -1,8 +1,5 @@
 package br.com.manygames.githubprovider.domain
 
-import javax.persistence.Entity
-import javax.persistence.Id
-
 data class Issue(
         val id: Int,
         val action: String,
